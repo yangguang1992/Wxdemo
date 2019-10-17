490bbafbbc18ec98acfb07e9ff4862365ce5b5af
李小磊 <962189096@qq.com>
2019-10-17 16:20:03 +0800

    知识点3绑定数据 列表展示 事件监听

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,9 +13,29 @@ Page({
     interval: 2000,
     duration: 500,
     previousMargin: 0,
-    nextMargin: 0
+    nextMargin: 0,
+    name:"home",
+    students:[{id:10,name:"张三",age:25},
+      { id: 11, name: "李四", age:26 },
+      { id: 12, name: "王五", age:27 }],
+    counter:0
+
   },
-  
+  handleBtenClick(){
+    //错误的 界面不会刷新
+    // this.data.counter +=1,
+
+    this.setData({
+counter:this.data.counter+1
+    })
+  },
+  handleSubtraction(){
+
+    this.setData({
+      
+      counter: this.data.counter - 1
+    })
+  },
   onLoad: function () {
    
   },
